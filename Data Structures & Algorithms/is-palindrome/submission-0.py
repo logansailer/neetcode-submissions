2c7be9a4af06d527df05b7d53abf63193922a8c7
Logan Sailer <144751209+logansailer@users.noreply.github.com>
2026-09-09 19:03:09 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alnum = ''.join(filter(str.isalnum, s))
+        alnum = alnum.lower()
+        for i in range(len(alnum)//2):
+            print(alnum[i], alnum[-(i+1)])
+            if alnum[i] != alnum[-(i+1)]:
+                return False
+        return True
+        
